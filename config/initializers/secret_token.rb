3c85723116af3de843c332f7feee70a2736a5842
Alexandre Gama <login+alexandregama@elo7.com>
2015-01-01 21:26:21 -0200

    First commit with scaffold room generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Colchonet::Application.config.secret_token = '62037cbd12dcabc474733872f4f035a5077fd2f7249ffc6d4b2bb553d72fde854c0fdfe4c8683e92331edc3fb47e27b15983d8e4342a9376c01180ec6507f379'
